adb07778152b96dc87ffa21cc1d9afcc62576e29
Neson <1385463@mail.ru>
2024-02-16 07:53:55 +0400

    First commit for example...

---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+
+
+function Footer() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = dd + '/' + mm + '/' + yyyy;
+    return (
+        <footer>
+            <p>&copy; {today}Your website name</p>
+        </footer>
+    );
+}
+
+export default Footer
